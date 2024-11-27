b5b5a1e0b9f18a2ffc8f7286f283276dd3dc97e8
José Fernando Resendiz López <56377740+Gerbo67@users.noreply.github.com>
2024-11-27 14:59:48 -0600

    Euler

--- index.js
+++ index.js
@@ -2,20 +2,16 @@
 // [ f(x, y) = 2y - 2x^2 + x - 3 ]
 const f = (x, y) => 2 * y - 2 * Math.pow(x, 2) + x - 3;
 
-// Metodo de Runge-Kutta de Cuarto Orden
-function rungeKutta4(x0, y0, h, steps) {
+// Metodo Euler
+function eulerMethod(x0, y0, h, steps) {
     let x = x0;
     let y = y0;
 
-    for (let i = 0; i < steps; i++) {
-        let k1 = h * f(x, y);
-        let k2 = h * f(x + h / 2, y + k1 / 2);
-        let k3 = h * f(x + h / 2, y + k2 / 2);
-        let k4 = h * f(x + h, y + k3);
-
-        y += (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+    for (let i = 0 ; i < steps; i++){
+        y += h * f(x, y);
         x += h;
     }
+
     return y;
 }
 
@@ -25,6 +21,6 @@ const y0 = 1.2;
 const h = 0.1;
 const steps = Math.floor(1 / h);
 
-// Obtener y(1) usando el metodo de Runge-Kutta de cuarto orden
-const y1_rk4 = rungeKutta4(x0, y0, h, steps);
-console.log(`y(1) Runge-Kutta respuesta: ${y1_rk4}`);
+// Obtener y(1) usando el metodo de Euler
+const y1_rk4 = eulerMethod(x0, y0, h, steps);
+console.log(`y(1) Euler respuesta: ${y1_rk4}`);
